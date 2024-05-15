df0e1229400bd4b5b44473d323a8e07dc72780df
arjun <127928367+arjun2159@users.noreply.github.com>
2024-05-15 21:19:56 +0530

    project completed

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,11 +6,17 @@ import Skeleton from "@mui/material/Skeleton";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import myImage from "../assets/images/ProjectImage1.png";
+import myImage2 from "../assets/images/ProjectImage2.png";
+import myImage3 from "../assets/images/ProjectImage3.png";
 
 const RowWrapper = styled(Row)`
   padding: 40px 0px 100px;
   background: black;
   color: white;
+`;
+const ColImage = styled(Col)`
+  justify-content: center;
+  display: flex;
 `;
 const RowWrapperHeader = styled.div`
   font-size: 48px;
@@ -21,10 +27,20 @@ const RowWrapperHeader = styled.div`
     font-size: 32px;
   }
 `;
-const ProjectImage = styled.img`
-  padding: 20px 70px;
+const RowWrapperBody = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  align-items: center;
   @media (max-width: 767px) {
-    padding: 10px;
+    display: block;
+  }
+`;
+const ProjectImage = styled.img`
+  align-self: flex-end;
+  @media (max-width: 767px) {
+    justify-content: center;
+    display: flex;
+    padding-left: 25px;
   }
 `;
 
@@ -36,27 +52,69 @@ export const Projects = ({ loading }) => {
       ) : (
         <RowWrapper className="p-md-5">
           <RowWrapperHeader>Projects</RowWrapperHeader>
-          <Row>
-            <Col xs={12} sm={6}>
+          <RowWrapperBody>
+            <ColImage xs={12} sm={6}>
               <ProjectImage
                 src={myImage}
                 alt="profile image"
                 className="img-fluid"
               />
-            </Col>
-            <Col xs={12} sm={6} className="mt-4">
-              <Card>
+            </ColImage>
+            <Col xs={12} sm={6} className="mt-4 d-sm-block d-md-flex justify-content-center">
+              <Card className="bg-black text-white">
                 <Card.Body>
-                  <Card.Title>01</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">Crypto Screener Application</Card.Subtitle>
-                  <Card.Text>
+                  <Card.Title style={{ fontSize: '48px'}}>01</Card.Title>
+                  <Card.Subtitle style={{ fontSize: '32px'}} className="mb-2">Crypto Screener Application</Card.Subtitle>
+                  <Card.Text style={{ fontSize: '16px'}}>
                     I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book.
                   </Card.Text>
                   <Card.Link href="#">Card Link</Card.Link>
                 </Card.Body>
               </Card>
             </Col>
-          </Row>
+          </RowWrapperBody>
+          <RowWrapperBody>
+            <Col xs={12} sm={6} className="mt-4 d-sm-block d-md-flex justify-content-center">
+              <Card className="bg-black text-white">
+                <Card.Body>
+                  <Card.Title style={{ fontSize: '48px'}}>02</Card.Title>
+                  <Card.Subtitle style={{ fontSize: '32px'}} className="mb-2">Crypto Screener Application</Card.Subtitle>
+                  <Card.Text style={{ fontSize: '16px'}}>
+                    I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book.
+                  </Card.Text>
+                  <Card.Link href="#">Card Link</Card.Link>
+                </Card.Body>
+              </Card>
+            </Col>
+            <ColImage xs={12} sm={6}>
+              <ProjectImage
+                src={myImage2}
+                alt="profile image"
+                className="img-fluid"
+              />
+            </ColImage>
+          </RowWrapperBody>
+          <RowWrapperBody>
+            <ColImage xs={12} sm={6}>
+              <ProjectImage
+                src={myImage3}
+                alt="profile image"
+                className="img-fluid"
+              />
+            </ColImage>
+            <Col xs={12} sm={6} className="mt-4 d-sm-block d-md-flex justify-content-center">
+              <Card className="bg-black text-white">
+                <Card.Body>
+                  <Card.Title style={{ fontSize: '48px'}}>01</Card.Title>
+                  <Card.Subtitle style={{ fontSize: '32px'}} className="mb-2">Crypto Screener Application</Card.Subtitle>
+                  <Card.Text style={{ fontSize: '16px'}}>
+                    I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book.
+                  </Card.Text>
+                  <Card.Link href="#">Card Link</Card.Link>
+                </Card.Body>
+              </Card>
+            </Col>
+          </RowWrapperBody>
         </RowWrapper>
       )}
     </>
